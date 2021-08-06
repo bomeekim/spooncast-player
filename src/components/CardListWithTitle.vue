34d6cc4ea55445492164c25cc511ff884d7ff67d
bomeekim <thespringg@naver.com>
2021-08-06 18:01:04 +0900

    카드 hover 시 좋아요 및 재생 횟수 노출

--- src/components/CardListWithTitle.vue
+++ src/components/CardListWithTitle.vue
@@ -1,19 +1,25 @@
 <template>
   <div>
     <v-row class="pa-5">
-      <p>{{ title }}</p>
+      <p class="text-xl-h6 text-lg-h6 text-subtitle-1 font-weight-medium">
+        {{ title }}
+      </p>
       <v-spacer />
-      <v-btn text>
-        더보기
+      <v-btn
+        class="pa-0"
+        icon
+      >
         <v-icon>
-          mdi-plus
+          mdi-chevron-right
         </v-icon>
       </v-btn>
     </v-row>
+
     <v-card
       class="d-flex flex-nowrap"
       width="inherit"
       flat
+      rounded
     >
       <flicking
         class="flicking"
@@ -25,26 +31,54 @@
           :key="index"
           class="card-panel"
         >
-          <v-card
-            flat
-            width="250"
-            height="280"
-            class="justify-center"
-            @click="$emit('click:item', item)"
-          >
-            <v-card-text class="pa-1">
-              <v-img
-                class="justify-center mx-auto rounded"
-                :src="item.img_url"
-                height="200"
-                width="200"
-              />
-            </v-card-text>
-            <v-card-actions class="flex-column">
-              <p>{{ item.title }}</p>
-              <p>{{ item.author.nickname }}</p>
-            </v-card-actions>
-          </v-card>
+          <v-hover v-slot="{ hover }">
+            <v-card
+              flat
+              width="250"
+              height="310"
+              class="justify-center inner-card"
+              :class="{ 'on-hover': hover }"
+              @click="$emit('click:item', item)"
+            >
+              <v-card-text>
+                <v-img
+                  class="justify-center mx-auto rounded mb-2"
+                  :src="item.img_url"
+                  height="220"
+                >
+                  <v-expand-transition>
+                    <div
+                      v-if="hover"
+                      class="hover-card
+                             d-flex
+                             text-h5
+                             justify-center
+                             align-center
+                             align-content-center"
+                    >
+                      <p>
+                        <span>
+                          <v-icon>mdi-heart</v-icon>
+                          {{ item.like_count }}
+                        </span>
+                        <span>
+                          <v-icon>mdi-play</v-icon>
+                          {{ item.play_count }}
+                        </span>
+                      </p>
+                    </div>
+                  </v-expand-transition>
+                </v-img>
+                <span
+                  class="d-inline-block text-truncate subtitle-1 font-weight-medium"
+                  style="max-width: 222px;"
+                >
+                  {{ item.title }}
+                </span>
+                <p>{{ item.author.nickname }}</p>
+              </v-card-text>
+            </v-card>
+          </v-hover>
         </div>
       </flicking>
     </v-card>
@@ -87,5 +121,19 @@ export default {
 
 p {
   margin: 0;
+}
+
+.card-text-title {
+  letter-spacing: -0.05rem !important;
+}
+
+.hover-card {
+  background-color: rgba(0, 0, 0, 0.6);
+  height: 100%;
+}
+
+.hover-card p,
+.hover-card .v-icon {
+  color: #eee;
 }
 </style>
